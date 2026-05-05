db1a5d3ab1878655bd52911bc24d2b8b23bfa7e1
Nweke Onyekachukwu <55864433+Neo-glitch@users.noreply.github.com>
2026-05-05 17:11:19 +0100

    feat: update app version to 1.0.4 and add Android vitals settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.neocalc.neocalc"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
